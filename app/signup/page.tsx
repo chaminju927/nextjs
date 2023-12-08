4b79505d4f9dd3ee3d78818563f706a8aa27620b
MINJU CHA <minjoo927@jness.co.kr>
2023-12-08 16:29:47 +0900

    .

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,55 +1,25 @@
-"use client";
+'use client';
 
-import {
-  useRef,
-  useState,
-  useMemo,
-  useCallback,
-  useEffect,
-  RefObject,
-} from "react";
-import Image from "next/image";
-import InputBox from "@/components/inputBox/InputBox";
-import Button from "@/components/button/button";
-import styles from "./signup.module.scss";
-import Step1 from "./step1";
-//import Step2 from "./step2";
-import Step3 from "./step3";
-
-interface Step1Ref {
-  inputRef: RefObject<HTMLInputElement>;
-}
-const checkLabel = [
-  { id: 0, name: "전체동의" },
-  { id: 1, name: "[필수] 서비스 이용약관 동의" },
-  { id: 2, name: "[필수] 개인정보 수집 및 이용 동의" },
-  { id: 3, name: "[필수] 만 14세 이상입니다." },
-  { id: 4, name: "[선택] 마케팅 정보 수신 동의" },
-];
+import { useRef, useState, RefObject, useCallback, useEffect } from 'react';
+import Button from '@/components/button/button';
+import styles from './signup.module.scss';
+import Step1, { Step1InputHandle } from './step1';
+import Step2, { Step2InputHandle } from './step2';
+import Checkbox from '@/components/checkBox/checkBox';
 
 const Signup = () => {
+  // Step 1 컴포넌트 핸들러
+  const refStep1 = useRef<Step1InputHandle>(null);
+  const refStep2 = useRef<Step2InputHandle>(null);
   const [step, setStep] = useState<number>(1);
-  // const inputRef = useRef<React.ForwardedRef<HTMLInputElement>[]>([]);
 
-  // const moveNext = () => {
-  //   //    inputRef.current.forEach((el) => console.log(el));
-  //   //console.log(inputRef.current['company'].value);
-  //   // if (step + 1 <= 3) {
-  //   //   setStep(step + 1);
-  //   // }
+  const [checkState, setCheckState] = useState<boolean>(false);
 
-  //   //eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  //   //@ts-expect-error
-  //   console.log(inputRef!.current.value);
-  // };
-  const step1Ref = useRef<Step1Ref>(null);
   const moveNext = () => {
-    const companyName = step1Ref.current?.inputRef.current?.value;
-    console.log("Company Name:", companyName);
-    // const values = inputRefs.current.map((ref) => ref.current?.value);
-    // console.log(values);
-
+    // Step1 함수 호출해서 입력값 가져오기
+    console.log(refStep1.current?.getData());
     if (step + 1 <= 3) {
+      // 유효성 검사 후 클릭 가능
       setStep(step + 1);
     }
   };
@@ -59,29 +29,39 @@ const Signup = () => {
       setStep(step - 1);
     }
   };
+  const submit = () => {
+    //유효성검사 및 체크박스 체크여부 확인후 submit
+    const data1 = refStep1.current?.getData();
+    const data2 = refStep2.current?.getData();
+    console.log(data1, data2);
+  };
+
+  const checkItemHandler = (checked: boolean) => {
+    checked ? setCheckState(true) : setCheckState(false);
+    console.log(checked);
+  };
 
   return (
     <div className={styles.area}>
       <div className={styles.container}>
         <h1 className={styles.signup_title}>회원가입</h1>
         <span className={styles.sub_title}>
-          No .1 근로시간관리 솔루션 MOffice를 이제 클라우드 서비스 Pine Work로
-          경험해보세요!
+          No .1 근로시간관리 솔루션 MOffice를 이제 클라우드 서비스 Pine Work로 경험해보세요!
         </span>
         <div className={styles.area_text}>
           <span className={styles.label}>{step} 단계</span>
         </div>
-        <div style={{ display: step === 1 ? "block" : "none" }}>
-          <Step1 ref={step1Ref} />
-          {/* <Step1 ref={inputRef} /> */}
+        <div style={{ display: step === 1 ? 'block' : 'none' }}>
+          <Step1 ref={refStep1} validation={true} />
+
           <div className={styles.nav_button_area}>
             <Button
               label="다음"
               customStyle={{
                 width: 320,
                 height: 42,
-                backgroundColor: "#1e334c",
-                color: "#fff",
+                backgroundColor: '#1e334c',
+                color: '#fff',
                 marginLeft: 80,
                 marginTop: 20,
               }}
@@ -89,23 +69,70 @@ const Signup = () => {
             />
           </div>
         </div>
-        <div style={{ display: step === 1 ? "none" : "block" }}>
-          {/* <Step2 /> */}
-          {/* <div className={styles.nav_check_area}>
-            <Checkbox label={checkLabel} iconType="CHECK_BOX" />
-          </div> */}
+        <div style={{ display: step === 1 ? 'none' : 'block' }}>
+          <Step2 ref={refStep2} validation={true} />
+          <div className={styles.nav_check_area}>
+            <Checkbox
+              name="chk_agree_all"
+              label="전체동의"
+              value="0"
+              iconType="CHECK_BOX"
+              isChecked={checkState}
+              checkItemHandler={checkItemHandler}
+            />
+            <Checkbox
+              name="chk_agree_all"
+              label="[필수] 서비스 이용약관 동의"
+              value="1"
+              iconType="CHECK_BOX"
+              isChecked={checkState}
+            />
+            <Checkbox
+              name="chk_agree_all"
+              label="[필수] 개인정보 수집 및 이용 동의"
+              value="2"
+              iconType="CHECK_BOX"
+              isChecked={checkState}
+            />
+            <Checkbox
+              name="chk_agree_all"
+              label="[필수] 만 14세 이상입니다."
+              value="3"
+              iconType="CHECK_BOX"
+              isChecked={checkState}
+            />
+            <Checkbox
+              name="chk_agree_all"
+              label="[선택] 마케팅 정보 수신 동의"
+              value="4"
+              iconType="CHECK_BOX"
+              isChecked={checkState}
+            />
+          </div>
           <div className={styles.nav_button_area}>
             <Button
               label="가입하기"
               customStyle={{
-                width: 320,
+                width: 150,
                 height: 42,
-                backgroundColor: "#1e334c",
-                color: "#fff",
+                backgroundColor: '#1e334c',
+                color: '#fff',
                 marginLeft: 80,
                 marginTop: 20,
               }}
-              onClick={moveNext}
+              onClick={submit}
+            />
+            <Button
+              label="이전"
+              customStyle={{
+                width: 150,
+                height: 42,
+                backgroundColor: '#1e334c',
+                color: '#fff',
+
+                marginTop: 20,
+              }}
+              onClick={movePrev}
             />
           </div>
         </div>
--- app/signup/step1.tsx
+++ app/signup/step1.tsx
@@ -2,79 +2,44 @@
 
 import InputBox from "@/components/inputBox/InputBox";
 import styles from "./signup.module.scss";
-import {
-  RefObject,
-  forwardRef,
-  useState,
-  ForwardedRef,
-  useImperativeHandle,
-  useRef,
-} from "react";
+import { useState, useRef, useEffect, RefObject } from "react";
 
-interface Step1Ref {
-  inputRef: RefObject<HTMLInputElement>;
-}
-const Step1 = forwardRef(({}, ref: ForwardedRef<Step1Ref>) => {
-  const inputRef = useRef<HTMLInputElement>(null);
+const Step1 = (moveNext: any) => {
+  const companyRef = useRef<HTMLInputElement>(null);
+  const businessNoRef = useRef<HTMLInputElement>(null);
+  const domainRef = useRef<HTMLInputElement>(null);
+  const partnerRef = useRef<HTMLInputElement>(null);
 
-  const [test4, setTest4] = useState<string>("");
-  const [test3, setTest3] = useState<string>("");
-  const [test2, setTest2] = useState<string>("");
-  const [test1, setTest1] = useState<string>("");
-
-  useImperativeHandle(
-    ref,
-    () => ({
-      inputRef,
-    }),
-    [inputRef]
-  );
   return (
     <div>
-      <form className={styles.form}>
+      <form className={styles.form} id="form">
         <InputBox
           type="text"
           name="company"
           placeholder="회사명을 입력해주세요"
           label="회사명*"
           maxLength={20}
-          ref={inputRef} // ref={(el) => {
-          //   //eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          //   //@ts-expect-error
-          //   inputRef!.current[0] = el;
-          //   //setTest4(e.target.value);
-          // }}
+          ref={companyRef}
           inputStyle={{ marginRight: "10%", width: 320, fontSize: 12 }}
         />
+
         <InputBox
           type="text"
-          name="companyNo"
-          value={test3}
+          name="businessNo"
           placeholder="000-00-000000"
           label="사업자번호*"
           maxLength={20}
-          onChangeHandler={(e) => {
-            //eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            //@ts-expect-error
-            inputRef1!.current[1] = e.target.value;
-            setTest3(e.target.value);
-          }}
+          ref={businessNoRef}
           inputStyle={{ marginRight: "10%", width: 200, fontSize: 12 }}
         />
         {/* <Button label="조회" /> */}
         <InputBox
           type="text"
           name="domain"
-          value={test2}
           placeholder="영문 소문자/숫자/- 만 사용하여 입력해주세요."
           label="도메인명*"
           maxLength={20}
-          onChangeHandler={(e) => {
-            //eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            //@ts-expect-error
-            inputRef1!.current[2] = e.target.value;
-            setTest2(e.target.value);
-          }}
+          ref={domainRef}
           inputStyle={{
             marginRight: "10%",
             width: 320,
@@ -82,26 +47,21 @@ const Step1 = forwardRef(({}, ref: ForwardedRef<Step1Ref>) => {
             fontSize: 12,
           }}
         />
-        {/* <Button /> */}
+
         <InputBox
           type="text"
           name="partner"
-          value={test1}
           placeholder="파트너사명으로 검색해주세요. (최소 2글자 이상 입력)"
           label="담당 파트너사"
           maxLength={20}
           minLength={2}
-          onChangeHandler={(e) => {
-            //eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            //@ts-expect-error
-            inputRef1!.current[3] = e.target.value;
-            setTest1(e.target.value);
-          }}
+          ref={partnerRef}
           inputStyle={{ marginRight: "10%", width: 320, fontSize: 12 }}
         />
       </form>
     </div>
   );
-});
+};
 
 export default Step1;
+//
